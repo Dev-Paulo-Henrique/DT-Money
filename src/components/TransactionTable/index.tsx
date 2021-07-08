6e7c5cb6e427c5fe928ad9e3a2dced922fcc334b
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-07-07 21:29:37 -0300

    DtMoney

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionTable(){
+  useEffect(() => {
+    api.get('transactions').then(response => console.log(response.data))
+  }, [])
+
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="widthdraw">-R$500,00</td>
+            <td>Casa</td>
+            <td>25/02/2021</td>
+          </tr>
+          <tr>
+            <td>Walmart</td>
+            <td className="widthdraw">-R$1.000,00</td>
+            <td>Celular</td>
+            <td>26/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
